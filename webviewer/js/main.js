f2f3738a3aab4010c5fe244e27be7804d28b5cf8
2Carrot10 <eleanorgrutz@gmail.com>
2026-06-10 22:10:10 -0700

    remove webview (move to github.com/2carrot10/griddiagrams-webview)

--- webviewer/js/main.js
+++
@@ -1,5 +0,0 @@
-import { transitionCanvasWithVlist, setCurrentVlist } from "./griddiagram/griddiagramAnim.js" ;
-var currentVlist = [[7, 3], [1, 4], [2, 6], [3, 10], [10, 9], [8, 0], [9, 5], [4, 7], [6, 1], [5, 8], [0, 2]];
-var nextVlist = [[7, 3], [1, 4], [8, 6], [3, 10], [10, 9], [2, 0], [9, 5], [4, 7], [6, 1], [5, 2], [0, 8]];
-setCurrentVlist(currentVlist);
-transitionCanvasWithVlist(nextVlist, .01);
